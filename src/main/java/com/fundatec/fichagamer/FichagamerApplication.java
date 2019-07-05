737d770a41a8a6d11ed26e7a98af5789f834595f
Kalium1993 <tassio_tln@hotmail.com>
2019-07-04 21:58:49 -0300

    first commit

---
+++ src/main/java/com/fundatec/fichagamer/FichagamerApplication.java
@@ -0,0 +1,13 @@
+package com.fundatec.fichagamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichagamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichagamerApplication.class, args);
+	}
+
+}
