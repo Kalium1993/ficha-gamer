52bc9fe1d05ed7250f8f553596ccab09d46a36e0
Kalium1993 <tassio_tln@hotmail.com>
2019-08-17 01:12:08 -0300

    usando cloud

--- src/main/java/com/fundatec/fichagamer/FichaGamerApplication.java
+++ src/main/java/com/fundatec/fichagamer/FichaGamerApplication.java
@@ -2,10 +2,12 @@ package com.fundatec.fichagamer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
 public class FichaGamerApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FichaGamerApplication.class, args);
 	}
--- src/main/java/com/fundatec/fichagamer/domain/FichaGamer.java
+++ src/main/java/com/fundatec/fichagamer/domain/FichaGamer.java
@@ -2,10 +2,12 @@ package com.fundatec.fichagamer.domain;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Document
 public class FichaGamer {
     @Id
     private ObjectId _id;
